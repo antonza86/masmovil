7d3ba75964c3fe369018d405dda71d0f3ffacf07
Anton Zverev <antonza86@gmail.com>
2018-06-29 14:54:29 +0200

    detail phone

--- src/component/PhoneDetailComponent.jsx
+++ src/component/PhoneDetailComponent.jsx
@@ -1,34 +1,81 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Jumbotron, Grid, Row, Col, Image, Button} from 'react-bootstrap';
+import { Jumbotron, Grid, Row, Col, Image, Button, Table} from 'react-bootstrap';
+
+import axios from 'axios';
 
 import './PhoneDetailComponent.css';
 
-const PhoneDetailComponent = (props) => {
-  console.log(props.match.params.model)
-  return (
-    // <div>
-    //   <h1>{ props.match.params.model }</h1>
-    //   <Link to='/'>Back</Link>
-    // </div>
-    <Grid>
-      <Row className="show-grid text-center">
-      <h1>{ props.match.params.model }</h1>
-        {/* {
-          this.state.phones.map(phone => (
-            <Col xs={12} sm={4} className="column">
-              <h3>{phone.title}</h3>
-              <Image src={phone.image} rounded />
-              <br />
-              <Link to={'/detail/' + phone.model }>
-                <Button bsStyle="primary">MORE INFO</Button>
-              </Link>
+class PhoneDetailComponent extends Component {
+  state = {
+    phone: {}
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/phones/' + this.props.match.params.model)
+      .then(res => {
+        const phone = res.data;
+        this.setState({ phone });
+      })
+  }
+  
+  render(){
+    return (
+      <Grid>
+        <Link to="/">
+          <Button bsStyle="primary" className="back_button" >Back</Button>
+        </Link>
+        <Jumbotron>
+          <h2>{ this.state.phone.title }</h2>
+          <p>{ this.state.phone.description }</p>
+          <Row className="show-grid text-center">
+            <Col md={4} xs={12}>
+              <Image src={ this.state.phone.image } rounded className="detail_image" />
             </Col>
-          ))
-        } */}
-      </Row>
-    </Grid>
-  )
+            <Col md={8} xs={12}>
+              <h2>Detalle</h2>
+              <Table striped bordered condensed hover>
+                <tbody>
+                  <tr>
+                    <td className="left_side">Color</td>
+                    <td>{ this.state.phone.color }</td>
+                  </tr>
+                  <tr>
+                    <td>Precio</td>
+                    <td>{ this.state.phone.price }</td>
+                  </tr>
+                  <tr>
+                    <td>Bateria</td>
+                    <td>{ this.state.phone.battery }</td>
+                  </tr>
+                  <tr>
+                    <td>Sistema operativo</td>
+                    <td>{ this.state.phone.os }</td>
+                  </tr>
+                  <tr>
+                    <td>Memoria Ram</td>
+                    <td>{ this.state.phone.memory_ram }</td>
+                  </tr>
+                  <tr>
+                    <td>Memoria</td>
+                    <td>{ this.state.phone.memory }</td>
+                  </tr>
+                  <tr>
+                    <td>Camera</td>
+                    <td>{ this.state.phone.camera }</td>
+                  </tr>
+                  <tr>
+                    <td>Processor</td>
+                    <td>{ this.state.phone.processor }</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+        </Jumbotron>
+      </Grid>
+    )
+  }
 }
 
 export default PhoneDetailComponent
